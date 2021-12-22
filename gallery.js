3e6c0f6caec80d2e00c140b0f3950600420a92e6
JovanaJovceska <92115180+JovanaJovceska@users.noreply.github.com>
2021-12-22 15:44:08 +0100

    .
    .

---
+++ gallery.js
@@ -0,0 +1,101 @@
+function addItem() {
+    var ul = document.getElementById("dynamic-listUName");
+    var ul1 = document.getElementById("dynamic-listCom");
+    var name = document.getElementById("uname");
+    var comment = document.getElementById("wText");
+    
+    var li = document.createElement("li");
+    li.setAttribute('id', name.value);
+    li.appendChild(document.createTextNode(name.value));
+    ul.appendChild(li);
+
+    var li1 = document.createElement("li");
+    li1.setAttribute('id', comment.value);
+    li1.appendChild(document.createTextNode(comment.value));
+    ul1.appendChild(li1);
+}
+
+function addItem1() {
+    var ul = document.getElementById("dynamic-listUName1");
+    var ul1 = document.getElementById("dynamic-listCom1");
+    var name = document.getElementById("uname1");
+    var comment = document.getElementById("wText1");
+    
+    var li = document.createElement("li");
+    li.setAttribute('id', name.value);
+    li.appendChild(document.createTextNode(name.value));
+    ul.appendChild(li);
+
+    var li1 = document.createElement("li");
+    li1.setAttribute('id', comment.value);
+    li1.appendChild(document.createTextNode(comment.value));
+    ul1.appendChild(li1);
+}
+
+function addItem2() {
+    var ul = document.getElementById("dynamic-listUName2");
+    var ul1 = document.getElementById("dynamic-listCom2");
+    var name = document.getElementById("uname2");
+    var comment = document.getElementById("wText2");
+    
+    var li = document.createElement("li");
+    li.setAttribute('id', name.value);
+    li.appendChild(document.createTextNode(name.value));
+    ul.appendChild(li);
+
+    var li1 = document.createElement("li");
+    li1.setAttribute('id', comment.value);
+    li1.appendChild(document.createTextNode(comment.value));
+    ul1.appendChild(li1);
+}
+
+function addItem3() {
+    var ul = document.getElementById("dynamic-listUName3");
+    var ul1 = document.getElementById("dynamic-listCom3");
+    var name = document.getElementById("uname3");
+    var comment = document.getElementById("wText3");
+    
+    var li = document.createElement("li");
+    li.setAttribute('id', name.value);
+    li.appendChild(document.createTextNode(name.value));
+    ul.appendChild(li);
+
+    var li1 = document.createElement("li");
+    li1.setAttribute('id', comment.value);
+    li1.appendChild(document.createTextNode(comment.value));
+    ul1.appendChild(li1);
+}
+
+function addItem4() {
+    var ul = document.getElementById("dynamic-listUName4");
+    var ul1 = document.getElementById("dynamic-listCom4");
+    var name = document.getElementById("uname4");
+    var comment = document.getElementById("wText4");
+    
+    var li = document.createElement("li");
+    li.setAttribute('id', name.value);
+    li.appendChild(document.createTextNode(name.value));
+    ul.appendChild(li);
+
+    var li1 = document.createElement("li");
+    li1.setAttribute('id', comment.value);
+    li1.appendChild(document.createTextNode(comment.value));
+    ul1.appendChild(li1);
+}
+
+function addItem5() {
+    var ul = document.getElementById("dynamic-listUName5");
+    var ul1 = document.getElementById("dynamic-listCom5");
+    var name = document.getElementById("uname5");
+    var comment = document.getElementById("wText5");
+    
+    var li = document.createElement("li");
+    li.setAttribute('id', name.value);
+    li.appendChild(document.createTextNode(name.value));
+    ul.appendChild(li);
+
+    var li1 = document.createElement("li");
+    li1.setAttribute('id', comment.value);
+    li1.appendChild(document.createTextNode(comment.value));
+    ul1.appendChild(li1);
+}
